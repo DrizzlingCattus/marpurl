37f4c22d1e458fadcb27db8bdd2316165162b188
DrizzlingCattus <hit0473@gmail.com>
2020-03-17 04:01:13 +0900

    Test: upload db 통합테스트 추가
    - testify suite을 사용하여 테스트들이 진행하기전에 setup할 수 있도록
    SetupSuite 기능을 사용하였다.
    - 테스트 환경 용도로 DB를 세팅할 수 있도록 만들었다.
    - testfixture 패키지를 통해서 더미 데이터가 잘 세팅되는지 테스트를
    수행하였다.

--- api/upload_test.go
+++ api/upload_test.go
@@ -1,26 +1,70 @@
 package api
 
 import (
+	"fmt"
+	"log"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
+	"marpurl/db"
+	"marpurl/env"
+	"marpurl/model"
+
+	"github.com/go-testfixtures/testfixtures/v3"
+	"github.com/jinzhu/gorm"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/suite"
 )
 
 type UploadRouterMock struct {
 	mock.Mock
 }
 
-func TestLoadUploadAPI(t *testing.T) {
-	w := NewWebFramework()
+type UploadDBTestSuite struct {
+	suite.Suite
+	DB       *gorm.DB
+	Fixtures *testfixtures.Loader
+}
 
+func (suite *UploadDBTestSuite) SetupSuite() {
+	env.Load(env.TestEnvFilename)
+	suite.DB = db.Connect()
+	suite.DB.AutoMigrate(&model.PPT{})
+
+	sdb := suite.DB.DB()
+	fixtures, err := testfixtures.New(
+		testfixtures.Database(sdb),           // You database connection
+		testfixtures.Dialect("mysql"),        // Available: "postgresql", "timescaledb", "mysql", "mariadb", "sqlite" and "sqlserver"
+		testfixtures.Directory("../fixture"), // the directory containing the YAML files
+	)
+	if err != nil {
+		log.Fatalf("fixture setting fail %v", err)
+	}
+	suite.Fixtures = fixtures
+	fmt.Println("setup suite")
+}
+
+func (suite *UploadDBTestSuite) TestUploadDB() {
+	suite.Fixtures.Load()
+
+	var ppt model.PPT
+	suite.DB.Where("name = ?", "afile").First(&ppt)
+	assert.Equal(suite.T(), ppt.DirPath, "/test/a")
+}
+
+func TestSuite(t *testing.T) {
+	suite.Run(t, new(UploadDBTestSuite))
+}
+
+func TestAcceptUploadAPI(t *testing.T) {
 	uploadRouter := NewUploadRouter("/api/upload")
+	// 라우터에 연결될 자원들을 인터페이스로 만들어서 Mock 테스트가 가능해야한다.
+
 	// 원하는 라우터만 연결할 수 있게 만들어야한다
+	w := NewWebFramework()
 	w.ConnectRouters(uploadRouter)
-	// 라우터에 연결될 자원들을 인터페이스로 만들어서 목 테스트가 가능해야한다.
-	// 여기서는 DB, Storage - 깨끗하게 초기화되어야한다. (테스트 환경으로 시작, 끝에)
 	req := httptest.NewRequest(http.MethodGet, "/api/upload/test", nil)
 	rec := httptest.NewRecorder()
 
@@ -29,4 +73,6 @@ func TestLoadUploadAPI(t *testing.T) {
 		assert.Equal(t, http.StatusOK, rec.Code)
 		assert.Equal(t, "upload test", rec.Body.String())
 	}
+
+	// 작업이 끝마친 후 DB, Storage - 깨끗하게 초기화되어야한다. (테스트 환경으로 시작, 끝에)
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module marpurl
 go 1.13
 
 require (
+	github.com/go-testfixtures/testfixtures/v3 v3.1.1
 	github.com/jinzhu/gorm v1.9.12
 	github.com/joho/godotenv v1.3.0
 	github.com/labstack/echo v3.3.10+incompatible
