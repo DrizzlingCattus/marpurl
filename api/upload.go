140b97fd232b51f15852cf18f849b785f1b30754
DrizzlingCattus <hit0473@gmail.com>
2020-03-19 03:35:46 +0900

    Feat: 여러 파일을 업로드할 수 있는 api 추가
    로컬 스토리지의 지정된 장소에 marp 파일들이 저장된다.

--- api/upload.go
+++ api/upload.go
@@ -2,35 +2,97 @@ package api
 
 import (
 	"fmt"
+	"io"
+	"mime/multipart"
 	"net/http"
+	"os"
 
 	"github.com/labstack/echo"
 )
 
 type UploadRouter struct {
 	*echo.Group
-	prefix string
+	Prefix string
 	wf     WebFramework
 }
 
-func test(c echo.Context) error {
+func (r *UploadRouter) Test(c echo.Context) error {
 	return c.String(http.StatusOK, "upload test")
+}
+
+func (r *UploadRouter) Upload(c echo.Context) error {
+	label := c.FormValue("label")
+
+	// Multipart form
+	form, err := c.MultipartForm()
+	if err != nil {
+		return err
+	}
+	files := form.File["files"]
+
+	for _, file := range files {
+		err := SaveToStorage(label, file)
+		if err != nil {
+			return c.String(http.StatusInternalServerError, "fail to save file")
+		}
+
+		// TODO: insert ppt db record
+	}
+
+	return c.String(http.StatusOK, "file upload success")
 }
 
 func (r *UploadRouter) Connect(w WebFramework) error {
 	fmt.Println("connecting upload router")
 
-	r.Group = w.Group(r.prefix)
+	r.Group = w.Group(r.Prefix)
 	r.wf = w
-	r.GET("test", test)
+	r.GET("test", r.Test)
+	r.POST("", r.Upload)
 	return nil
 }
 
-func NewUploadRouter(prefix string) *UploadRouter {
+const (
+	StorageDirPath = "~/.marpurl/storage/"
+	DefaultDirPerm = 0755
+)
+
+// TODO: abstract storage layer for using different storages
+func SaveToStorage(label string, file *multipart.FileHeader) error {
+	// SaveToStorage use localstorage temporary
+	dirpath := StorageDirPath + label
+	err := os.MkdirAll(dirpath, DefaultDirPerm)
+	if os.IsExist(err) {
+		fmt.Printf("%s is already exist\n", label)
+	}
+
+	// Source
+	src, err := file.Open()
+	if err != nil {
+		return err
+	}
+	defer src.Close()
+
+	// Destination
+	dst, err := os.Create(dirpath + file.Filename)
+	if err != nil {
+		return err
+	}
+	defer dst.Close()
+
+	// Copy
+	if _, err = io.Copy(dst, src); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func NewUploadRouter() *UploadRouter {
 	r := &UploadRouter{
 		Group:  nil,
 		wf:     nil,
-		prefix: prefix,
+		Prefix: "/api/v1/upload",
 	}
 	return r
 }
